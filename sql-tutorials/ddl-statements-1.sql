c5691e6074307558e08523bbc8e7498dd7844d28
waseem123 <waseem_6957@yahoo.com>
2026-07-02 19:03:41 +0530

    DDL STATEMENTS

---
+++ sql-tutorials/ddl-statements-1.sql
@@ -0,0 +1,21 @@
+create database employee_management;
+use employee_management;
+
+CREATE TABLE tbl_employee(
+	emp_id INT PRIMARY KEY AUTO_INCREMENT,
+	emp_name varchar(30) NOT NULL,
+	emp_age int,
+	emp_salary int
+);
+
+CREATE TABLE tbl_dept(
+	dept_id INT PRIMARY KEY AUTO_INCREMENT,
+	dept_name varchar(30) NOT NULL,
+	dept_building varchar(1)
+);
+
+show tables;
+
+describe tbl_dept;
+
+  
