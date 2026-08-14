2ea62b3fbaa13559c870227c00aab9c516f989c9
waseem123 <waseem_6957@yahoo.com>
2026-08-14 19:08:55 +0530

    JOINS

--- sql-tutorials/foreign-key-revision.sql
+++ sql-tutorials/foreign-key-revision.sql
@@ -39,14 +39,66 @@ INSERT INTO tbl_product (product_name,category_id)
 INSERT INTO tbl_product (product_name,category_id)
     VALUES
     ('LED Monitor',101);
+    
 SELECT * FROM tbl_product;
 
 SELECT * FROM tbl_product as p
 JOIN tbl_category as c
 ON p.category_id = c.category_id
 WHERE p.category_id = 101
-order by p.product_id
-;
+order by p.product_id;
 
 UPDATE tbl_product SET product_price = 13000
 	WHERE product_id = 11;
+
+SELECT p.product_id,
+	p.product_name,
+    c.category_name
+FROM tbl_product as p
+JOIN tbl_category as c
+ON p.category_id = c.category_id
+order by p.product_name;
+
+SELECT p.product_id,
+	p.product_name,
+    p.product_price,
+    c.category_name
+FROM tbl_product as p€
+JOIN tbl_category as c
+ON p.category_id = c.category_id
+order by c.category_name,p.product_price;
+
+SELECT p.product_id,
+	p.product_name,
+    p.product_price,
+    c.category_name
+FROM tbl_product as p
+JOIN tbl_category as c
+ON p.category_id = c.category_id
+WHERE c.category_name IN ('Furniture','Stationary');
+-- WHERE c.category_name='Furniture'
+-- OR c.category_name='Stationary';
+
+
+--  Multi-table join
+
+Create table tbl_shop(
+	shop_id int primary key auto_increment,
+	shop_name varchar(30),
+	shop_city varchar(30)
+);
+
+Insert into tbl_shop(shop_name,shop_city)
+	VALUES('ABC Electronics','Solapur'),
+    ('PQR Furnitures','Solapur'),
+    ('LMN Electronics','Solapur'),
+    ('XYZ Stationaries','Solapur');
+    
+    
+ALTER TABLE tbl_product
+	ADD COLUMN shop_id int,
+	ADD FOREIGN KEY (shop_id) references tbl_shop(shop_id);
+    
+SELECT * FROM tbl_product;
+UPDATE tbl_product set shop_id = 4
+WHERE shop_id IS NULL;
