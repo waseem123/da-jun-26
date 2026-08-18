7114c69e7b685e49fb136a7b6e637734d6132ef0
waseem123 <waseem_6957@yahoo.com>
2026-08-18 18:56:02 +0530

    RIGHT JOIN

---
+++ sql-tutorials/SQL-right-join.sql
@@ -0,0 +1,36 @@
+USE employee_management;
+SHOW TABLES;
+
+SELECT * FROM tbl_employee;
+SELECT * FROM tbl_dept;
+
+ALTER TABLE tbl_employee
+	ADD COLUMN dept_id int,
+    ADD FOREIGN KEY(dept_id) references tbl_dept(dept_id);
+    
+UPDATE tbl_employee SET dept_id = 101
+	WHERE emp_id = 10;
+    
+SELECT e.emp_id,
+	e.emp_name,
+    d.dept_name
+FROM tbl_employee as e
+RIGHT JOIN tbl_dept as d
+ON e.dept_id = d.dept_id;
+
+SELECT COALESCE(e.emp_id,0),
+	COALESCE(e.emp_name,'NA'),
+    d.dept_name
+FROM tbl_employee as e
+RIGHT JOIN tbl_dept as d
+ON e.dept_id = d.dept_id
+ORDER BY e.emp_id;
+
+SELECT COUNT(e.emp_id),
+	d.dept_name
+FROM tbl_employee as e
+RIGHT JOIN tbl_dept as d
+ON e.dept_id = d.dept_id
+GROUP BY d.dept_name
+HAVING COUNT(e.emp_id)>2
+ORDER BY COUNT(e.emp_id);
