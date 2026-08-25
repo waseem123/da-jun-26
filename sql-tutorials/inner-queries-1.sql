44f5e3a912d92971ff5d9bc38295990ef3a76d81
waseem123 <waseem_6957@yahoo.com>
2026-08-25 09:07:40 +0530

    Inner-Queries

--- sql-tutorials/inner-queries-1.sql
+++ sql-tutorials/inner-queries-1.sql
@@ -20,3 +20,25 @@ SELECT * FROM tbl_product
 		SELECT product_price FROM tbl_product 
 			WHERE product_name = 'Sofa'
     );
+    
+SELECT s.shop_id,s.shop_name,s.shop_city
+	FROM tbl_category as c
+    JOIN tbl_product as p
+    ON c.category_id = p.category_id
+    JOIN tbl_shop as s
+    ON s.shop_id = p.shop_id
+    WHERE c.category_name != 'Stationary';
+    
+    
+SELECT product_name,product_price
+	FROM tbl_product WHERE product_price > (
+		SELECT avg(product_price) FROM tbl_product
+    );
+    
+SELECT product_name,product_price
+	FROM tbl_product WHERE product_price > (
+		SELECT avg(product_price) FROM tbl_product p
+			JOIN tbl_category as c
+            ON c.category_id = p.category_id
+            WHERE c.category_name='Furniture'
+    );
